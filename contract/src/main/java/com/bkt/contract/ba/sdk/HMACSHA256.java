50c57d258c24e2b039f49aaebf0c47d5c8e33dfb
XXF <xuanyouwu@163.com>
2020-12-18 17:23:59 +0800

    feat:增加HMACSHA256 工具类

---
+++ contract/src/main/java/com/bkt/contract/ba/sdk/HMACSHA256.java
@@ -0,0 +1,50 @@
+package com.bkt.contract.ba.sdk;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * @Description: HMACSHA256 工具类
+ * @Author: XGod
+ * @CreateDate: 2020/12/17 20:47
+ */
+public class HMACSHA256 {
+    /**
+     * 将加密后的字节数组转换成字符串
+     *
+     * @param b 字节数组
+     * @return 字符串
+     */
+    private static String byteArrayToHexString(byte[] b) {
+        StringBuilder hs = new StringBuilder();
+        String stmp;
+        for (int n = 0; b != null && n < b.length; n++) {
+            stmp = Integer.toHexString(b[n] & 0XFF);
+            if (stmp.length() == 1)
+                hs.append('0');
+            hs.append(stmp);
+        }
+        return hs.toString().toLowerCase();
+    }
+
+    /**
+     * sha256_HMAC加密
+     *
+     * @param message 消息
+     * @param secret  秘钥
+     * @return 加密后字符串
+     */
+    public static String sha256_HMAC(String message, String secret) {
+        String hash = "";
+        try {
+            Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+            SecretKeySpec secret_key = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+            sha256_HMAC.init(secret_key);
+            byte[] bytes = sha256_HMAC.doFinal(message.getBytes());
+            hash = byteArrayToHexString(bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return hash;
+    }
+}
